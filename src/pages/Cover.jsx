56453b322bb101d0aa87d9a25da8a9fb117dfcd8
Seckin Ozturk <franseck102@gmail.com>
2025-02-12 11:19:20 -0500

    covar page

--- src/pages/Cover.jsx
+++ src/pages/Cover.jsx
@@ -13,8 +13,14 @@ const Cover = ({coverImg , title}) => {
 
   return (
     <div className='h-full bg-black ' style={coverImgStyle}>
-        <div className='h-full flex justify-center items-center'>
-            <h1 className='text-primary text-5xl font-bold font-outline-2'>{title}</h1>
+        <div className='h-full flex justify-center '>
+                    <div className='mt-2'>
+     <p className='text-2xl md:text-3xl font-fred text-black/80  '>A Journey to</p>
+    <p className='text-black/75 font-fred  absolute right-2 text-4xl tracking-widest'>My Portfolio</p>
+<div className='flex flex-col justify-center items-center '>
+    <p className='font-fred absolute bottom-6 text-5xl '>Seckin Ozturk </p>
+</div>
+</div>
         </div>
 
 
